aae71504a55049491cf376abcf4e341ac05581cf
RyougiS <deathzmh@hotmail.com>
2017-12-16 22:24:04 +0800

    3

--- interview/functionTest.js
+++ interview/functionTest.js
@@ -1,3 +1,4 @@
+
 //不定参数求和 非Number类过滤
 
 function sum() {
@@ -27,11 +28,11 @@ nodes.forEach((a)=>{
 	if(a.nodeName == "A"){
 		a.addEventListener("click",function(){ return alert (a.text) },false)
 	}
-})
+});
 
 //字符串逆序
 function reversText(str) {
-  return str.split("").reverse().join("")
+  return str.split("").reverse().join("");
 }
 var reverse = function( str ){
   var newStr = '', i = str.length;
@@ -40,7 +41,7 @@ var reverse = function( str ){
    }
    return newStr;
 };
-reverse('abcde')
+reverse('abcde');
 
 //物体平移Animation
 
@@ -57,4 +58,36 @@ function test(n) {
     }
   }
   return list;
+}
+
+//求一个字符串的字节长度
+var lenFor = function(str){
+  var byteLen=0,len=str.length;
+  if(str){
+    for(var i=0; i<len; i++){
+      if(str.charCodeAt(i)>255){
+        byteLen += 2;
+      }else{
+        byteLen++;
+      }
+    }
+    return byteLen;
+  }else{
+    return 0;
+  }
+}  //一般方法
+function getBytesLength(str) {
+  // 在GBK编码里，除了ASCII字符，其它都占两个字符宽
+  return str.replace(/[^\x00-\xff]/g, 'xx').length;
+}
+
+//100开始 连除2 10次
+function down(n) {
+  var height = 100,Sn = 0;
+  for (var i = 0; i < n; i++) {
+    Sn = Sn + height;
+    height = height/2;
+    Sn = Sn + height;
+  }
+  return "总距离Sn:"+Sn+",高度height:"+height;
 }
